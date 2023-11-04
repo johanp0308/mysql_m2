dc90f608f7215244bb1898d4e4983dba3e504e15
johanp0308 <sebastian-johan1@hotmail.com>
2023-11-04 18:26:36 -0500

    feat: :sparkles: Adding 'incripcion' table to model

--- query.sql
+++ query.sql
@@ -1,5 +1,5 @@
 
-
+DROP TABLE IF EXISTS Inpscripcion;
 DROP TABLE IF EXISTS Asistencia;
 DROP TABLE IF EXISTS Estudiante;
 DROP TABLE IF EXISTS Curso;
@@ -12,6 +12,14 @@ DROP TABLE IF EXISTS Facultad;
 
 
 
+
+CREATE TABLE IF NOT EXISTS Inpscripcion(
+    id INT UNSIGNED NOT NULL,
+    fk_estudiante_id INT UNSIGNED NOT NULL,
+    fk_curso_id INT UNSIGNED NOT NULL
+);
+
+ALTER TABLE ADD CONSTRAINT PK_Inscripcion_id PRIMARY KEY (id);
 
 CREATE TABLE IF NOT EXISTS Asistencia(
     id INT UNSIGNED NOT NULL,
@@ -163,9 +171,13 @@ ALTER TABLE Session_Academica
     ADD CONSTRAINT FK_SessAcad_Estudiante FOREIGN KEY (fk_estudiante_id) REFERENCES Estudiante(id),
     ADD CONSTRAINT FK_SessAcad_Departamento FOREIGN KEY (fk_departamento_id) REFERENCES Departamento(id);
 
+ALTER TABLE Inpscripcion
+    ADD CONSTRAINT FK_Inscripcion_Estudiante FOREIGN KEY (fk_estudiante_id) REFERENCES Estudiante(id),
+    ADD CONSTRAINT FK_Inscripcion_Curso FOREIGN KEY (fk_curso_id) REFERENCES Curso(id);
+
 -- Insercion de Datos
 
--- Facultad
+-- Consultas hechas
 INSERT INTO Facultad VALUES 
 (1,'Ciencias e ingienerias','3202818465','Edificio A'),
 (2, 'Artes y humanidades', '3202832465', 'Edificio B'),
@@ -173,7 +185,7 @@ INSERT INTO Facultad VALUES
 (4, 'Ciencias económicas ', '320281823', 'Edificio D'),
 (5, 'Ciencias sociales', '549839', 'Edificio E');
 
--- Depatamento
+-- Consultas Hechas
 INSERT INTO Departamento VALUES 
 (1,'Ingieneria Sistemas',1,'Ofc. 302','sistemas@edu.co','3208489'),
 (2, 'Emprendiminto ', 4, 'Ofc. 203','emprree@edu.co','89798'),
@@ -188,6 +200,7 @@ INSERT INTO Profesores VALUES
 (100654, 'María', 'López', 4, 'marialopez@universidad.edu.co', '+57319555', 'calle 4 #4-4'),
 (100655, 'Andrés', 'Martínez', 5, 'andresmartinez@universidad.edu.co', '+57319444', 'calle 5 #5-5');
 
+-- Consultas Hechas
 INSERT INTO Pariente VALUES
 (1225689741,'Johan Sebastian','Pachon Mojica','3214845641','jspachonm@ret.com'),
 (1225689721, 'Andrea', 'Gómez Mojica', '3204845641', 'andgomez@ret.com'),
@@ -195,6 +208,7 @@ INSERT INTO Pariente VALUES
 (1225689781, 'María', 'Mojica García', '3234845641', 'mariamojica@ret.com'),
 (1225689742, 'Andrés', 'Pachon Gómez', '3244845641', 'andpachon@ret.com');
 
+-- Consultas Hechas
 INSERT INTO Estudiante VALUES
 (51241,'Pedro','Jimenez','2001-07-12',1225689741,'pedro@departamento.edu.co'),
 (1312312, 'Pedro', 'Jimenez', '2001-07-12', 1225689721, 'pedr@departamento.edu.co'),
@@ -202,12 +216,15 @@ INSERT INTO Estudiante VALUES
 (1312314, 'Juan', 'Garcia', '1999-10-20', 1225689781, 'juan@departamento.edu.co'),
 (1312315, 'Ana', 'Perez', '2002-01-05', 1225689742, 'ana@departamento.edu.co');
 
+-- Consultas hechas
 INSERT INTO Curso VALUES
 (1,'Algebra Superior','2023-02-7','2023-07-01',40,1,100651),
 (2, 'Gestion', '2023-03-10', '2023-07-15', 40, 2, 100652),
 (3, 'Hidraulicos', '2023-03-10', '2023-07-15', 20, 3, 100653),
 (4, 'Micro-Producion', '2023-03-10', '2023-07-15', 35, 4, 100654),
 (5, 'Patogenos', '2023-03-10', '2023-07-15', 35, 5, 100655);
+
+-- Consultas hechas
 INSERT INTO Session_Academica VALUES
 (1,'Mañana','2023-02-8',51241,1,'Pasi-Salvo'),
 (2,'Tarde','2023-02-8',1312312,2,'Pasi-Salvo'),
@@ -215,6 +232,7 @@ INSERT INTO Session_Academica VALUES
 (4,'Noche','2023-02-8',1312314,1,'Pasi-Salvo'),
 (5,'Mañana','2023-07-10',1312315,2,'Pendiente');
 
+-- Consultas hechas
 INSERT INTO Examen VALUES
 (1,'2023-06-28','4.5','Aprobado',51241,1),
 (2,'2023-06-28','3.0','Aprobado',1312312,2),
@@ -223,11 +241,22 @@ INSERT INTO Examen VALUES
 (5,'2023-06-28','4.9','Aprobado',1312315,5);
 
 
-USE institution;
+INSERT INTO Inpscripcion VALUES
+(1,51241,1),
+(2,1312312,2),
+(3,1312313,3),
+(4,1312314,4),
+(5,1312315,5);
 
 
--- Consultas
 
+/*
+
+    Consultas de la base de Datos
+
+*/
+
+/* Session_Academica */
 -- Estudiante que esten Pasi Salvo con su id y fecha de inico
 SELECT tipo_session as Jornada,fecha_session as Inicio de la Session, fk_estudiante_id as Estudiante,
 IF(estado='Pendiente','No cumple','Bien')
@@ -237,14 +266,78 @@ FROM Session_Academica as Sec
 -- Estudiantes que terminaron las session por jornadas.
 SELECT tipo_session as Jornada, COUNT(tipo_session) as Estudiantes FROM Session_Academica GROUP BY tipo_session;
 
+/* Facultad */
+
 -- Facultad buscadas por sus ubicaciones
 SELECT ubicacion as Edficio, facultad_title as Facultad FROM Facultad;
 
 -- Facultad especifica de su ubicacion
 SELECT facultad_title as Facultad FROM Facultad WHERE ubicacion='Edificio A';
 
+/* Examen */
+
 -- Cantidad estudiantes aprobados y reprobados
 SELECT COUNT(estado) as Estudiantes_Aprobados FROM Examen WHERE estado='Aprobado' GROUP BY estado;
 SELECT COUNT(estado) as Estudiantes_Aprobados FROM Examen WHERE estado='Reprobado' GROUP BY estado;
 
+-- Agrupa Cantidad de examenes de un curso
 SELECT COUNT(fk_curso_id) as Curso_Id FROM Examen WHERE  GROUP BY fk_curso_id; 
+
+
+/* Curso */
+
+/* Muestra el nombre del curso su departamenot y nombre del profe */
+SELECT
+    c.titulo as Curso, 
+    d.nombre_departamento as Departamento, 
+    CONCAT(p.nombres,' ',p.apellidos) as Profesor 
+FROM Curso c 
+JOIN Profesores p ON c.fk_profesor_id = p.id 
+JOIN Departamento d ON c.fk_departament_id = d.id;
+
+/* Muestra el nombre del curso su departamenot y nombre del profesor a los cursos con capacidad menor 40 */
+SELECT 
+    c.titulo as Curso, 
+    d.nombre_departamento as Departamento, 
+    CONCAT(p.nombres,' ',p.apellidos) as Profesor 
+FROM Curso c 
+JOIN Profesores p ON c.fk_profesor_id = p.id 
+JOIN Departamento d ON c.fk_departament_id = d.id
+WHERE c.max_cupo < 40;
+
+
+/* Estudiantes */
+
+/* Muestre los estudiantes y su curso */
+SELECT CONCAT(e.nombre,' ',e.apellidos) as Nombre_Estudiante, c.titulo as Curso
+FROM Estudiante e
+JOIN Inpscripcion i ON e.id = i.fk_estudiante_id
+JOIN Curso c ON i.fk_curso_id = c.id;
+
+/* Muestra los Estudiantes Aprobados gracias al examen */
+
+SELECT CONCAT(e.nombre,' ',e.apellidos) as Estudiante, c.titulo as Curso ,ex.estado as Estado
+FROM Estudiante e
+JOIN Examen ex ON e.id = ex.fk_estudiante_id;
+
+/* Departamento */
+-- Muestra la informacion de un departamento
+SELECT * FROM Departamento WHERE id = 3;
+
+-- Muestra los departamentos que pertenecen a una facultad 
+SELECT * FROM Departamento WHERE fk_Facultad_id = 1;
+
+/* Pariente */
+-- Muestra la informacion de un pariente 
+SELECT * FROM Pariente WHERE id = 1225689741;
+
+--Muestra el pariente asociado a un estudiante
+SELECT * FROM Pariente WHERE id IN (SELECT pariente_id FROM Estudiante WHERE id = 51241);
+
+/* Profesores*/ 
+
+-- Muestra la informacion de un profesor
+SELECT * FROM Profesores WHERE id = 100653;
+
+-- Muestra todos los profesores que pertenecen a un departamento
+SELECT * FROM Profesores WHERE fk_departamento_id = 1;
